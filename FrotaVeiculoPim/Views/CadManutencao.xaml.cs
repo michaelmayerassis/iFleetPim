85849b9a942b23e9b6a5c9c485682f13824380a1
MICHAEL ASSIS <michael.assis01@etec.sp.gov.br>
2019-06-03 13:04:16 -0300

    msgaqui

--- FrotaVeiculoPim/Views/CadManutencao.xaml.cs
+++ FrotaVeiculoPim/Views/CadManutencao.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
-
+//aaaaaaaa
 namespace FrotaVeiculoPim.Views
 {
     /// <summary>
